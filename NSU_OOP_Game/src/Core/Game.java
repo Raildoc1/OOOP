e8e38210c9e634b6e303e9c2a4c817609959a250
Raildoc1 <Kirill1056@list.ru>
2020-03-04 22:45:45 +0700

    Game

--- NSU_OOP_Game/src/Core/Game.java
+++ NSU_OOP_Game/src/Core/Game.java
@@ -1,4 +1,4 @@
-package Core;
+package core;
 
 import javax.swing.*;
 import java.awt.*;
@@ -7,10 +7,14 @@ public class Game extends JFrame {
 
     private final String APP_NAME = "Game";
 
-    private int WIN_WIDTH = 300;
-    private int WIN_HEIGHT = 300;
+    private int WIN_WIDTH = 250;
+    private int WIN_HEIGHT = 350;
 
     public Game () {
+        initUI();
+    }
+
+    private void initUI() {
         add(new GamePanel());
 
         setSize(WIN_WIDTH, WIN_HEIGHT);
